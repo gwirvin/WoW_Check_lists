10ebc8248d816777528a0487c7d8d330e5179ce5
gwirvin <grantirvin@aol.com>
2018-05-18 15:30:06 -0400

    Another update for adding in links

--- links/addforlinks.php
+++ links/addforlinks.php
@@ -39,7 +39,7 @@ $link_url_err = "";
 $cat_email = $_SESSION['user_email'];
 $cat_owner = $_SESSION['user_id'];
 $cat_user = $_SESSION['user_first'];
-
+$link_owner = $cat_owner;
 //includes
 include "../cats.php";
 
@@ -64,8 +64,14 @@ $cat_query = mysqli_query ($links_conn, $cat_gather_sql);
 <h2>Adding a Category for <?php echo $_SESSION['user_first']?>'s Links Page</h2>
 <div id="container">
 <form action="./insert_cat.php" name="add_cat" method="POST">
-<div class="form-group"><label><font color="FFFFFF">Category Name:<sup>*</sup></label><input type="text" name="cat_name"  width="50em" value=""><input type="hidden" name="cat_owner" value="<?php print $cat_owner; ?>"><input type="submit" name="insert" value="ADD"></div>
+<div class="form-group"><label><font color="000000">Category Name:<sup>*</sup></label><input type="text" name="cat_name"  width="50em" value=""><input type="hidden" name="cat_owner" value="<?php print $cat_owner; ?>"><input type="submit" name="insert" value="ADD"></div>
 <p />
+<center>
+<h2>Adding a Category for <?php echo $_SESSION['user_first']?>'s Links Page</h2>
+<div id="container">
+<form action="./insert_link.php" name="add_link" method="POST">
+<div class="form-group"><label><font color="000000">Link Name:<sup>*</sup></label><input type="text" name="link_name" width="20em" value=""><label><font color=000000">Link Type:<sup>*</sup><select name="link_type"><option value="external">External Site</option><option value="internal">Internal Page</option><input type="hidden" name="link_owner" value="<?php print $link_owner;?>">
+<div class="form-group"><label>Link Address:<sup>*</sup><input type="url" name="link_url"><input type="submit" name="insert_link" value="Add Link"></div>
 <p />
 <p />
 </div>
