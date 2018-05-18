00316d3de5bff2b95efb7e1678ad4956ad898056
gwirvin <grantirvin@aol.com>
2018-05-18 16:31:23 -0400

    Cleaning missing end tags

--- links/addforlinks.php
+++ links/addforlinks.php
@@ -69,6 +69,8 @@ while ($cat_row = mysqli_fetch_array($cat_query))
 <div id="container">
 <form action="./insert_cat.php" name="add_cat" method="POST">
 <div class="form-group"><label><font color="000000">Category Name:<sup>*</sup></font></label><input type="text" name="cat_name"  width="50em" value=""><input type="hidden" name="cat_owner" value="<?php print $cat_owner; ?>"><input type="submit" name="insert" value="ADD"></div>
+</form>
+</div>
 <p />
 <center>
 <h2>Adding a Link for <?php echo $_SESSION['user_first']?>'s Links Page</h2>
@@ -84,6 +86,7 @@ while ($cat_row = mysqli_fetch_array($cat_query))
 <div class="form-group">
 <label><font color="000000">Link Category:<sup>*</sup></font></label><select name="link_cat"><?php print $cat_opts."\n"?></select>
 <label><font color="000000">Link Address:<sup>*</sup></font></label><input type="url" name="link_url"><input type="submit" name="insert_link" value="Add Link"></div>
+</div>
 <p />
 <p />
 </div>
