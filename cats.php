fec80d192081e97c2ea9c4ef62e54edc74618037
gwirvin <grantirvin@aol.com>
2018-05-21 13:42:49 -0400

    Setting in a home and logout to bookend links and nav buttons

--- cats.php
+++ cats.php
@@ -22,7 +22,7 @@ $blizz_locale = "locale=en_US";
 
 // Navigation table
 $nav_link_sql = mysqli_query($links_conn, "SELECT link_type, link_name, link_url FROM links WHERE link_cat=\"Navigation\" AND link_owner=\"".$_SESSION['user_id']."\" ORDER BY link_name") or die(mysqli_error());
-$nav_table="<center>\n<div id=\"table-nav\">\n<table>\n<center>\n<tr>\n\t<td><center><form name=\"links\" method=\"POST\" action=\"/links/links.php\"><input type=\"SUBMIT\" value=\"".$_SESSION['user_first']."'s Links\"></form></td>\n";
+$nav_table="<center>\n<div id=\"table-nav\">\n<table>\n<center>\n<tr>\n\t<td><center><form name=\"home\" method=\"POST\" action=\"index.php\"><input type=\"SUBMIT\" value=\"Go Home\">i</form></td>\n\t<td><center><form name=\"links\" method=\"POST\" action=\"/links/links.php\"><input type=\"SUBMIT\" value=\"".$_SESSION['user_first']."'s Links\"></form></td>\n";
 while ($nav_link_row = mysqli_fetch_array($nav_link_sql))
 	{
 	if ($nav_link_row['link_type'] == "internal")
@@ -34,5 +34,5 @@ while ($nav_link_row = mysqli_fetch_array($nav_link_sql))
 		$nav_table .= "\t<td><center><form name=\"".$nav_link_row['link_name']."\"method=\"POST\" action=\"".$nav_link_row['link_url']."\" target=\"_blank\"><input type=\"SUBMIT\" value=\"".$nav_link_row['link_name']."\"></form></td>\n";
 		}
 	}
-$nav_table .= "</tr>\n</center>\n</table>\n</div>\n</center>\n<hr />\n";
+$nav_table .= "\t<td><form name=\"logout\" method=\"POST\" action=\"logout.php\"><input value=\"Logout\" type=\"submit\"></form></td>\n</tr>\n</center>\n</table>\n</div>\n</center>\n<hr />\n";
 ?>
