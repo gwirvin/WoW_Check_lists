d3a7a90974f5727a16f3e8647fd7938b3d9c0de1
gwirvin <grantirvin@aol.com>
2018-05-23 14:48:08 -0400

    tyring to automatically seat internal vs external

--- links/insert_link.php
+++ links/insert_link.php
@@ -10,8 +10,16 @@ $link_name = ucwords($_POST['link_name']);
 $link_cat = $_POST['link_cat'];
 $link_owner = $_POST['link_owner'];
 $link_url = $_POST['link_url'];
-$link_type = $_POST['link_type'];
+$link_type = "";
 
+if (strpos($link_url, 'grantsgrabbag') !== false)
+{
+	$link_type= "internal";
+}
+else
+{
+	$link_type= "external";
+}
 $link_ins_sql = ("INSERT INTO links (link_name, link_cat, link_owner, link_url, link_type) VALUES (\"".$link_name."\", \"".$link_cat."\", \"".$link_owner."\", \"".$link_url."\", \"".$link_type."\")");
 $link_insert = mysqli_query($links_conn, $link_ins_sql);
 
