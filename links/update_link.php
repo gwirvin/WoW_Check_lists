48da307b1c0ec7a2d9bbb1f2d505e0d48186936e
gwirvin <grantirvin@aol.com>
2018-05-23 15:39:23 -0400

    Inital add of update_link.php

---
+++ links/update_link.php
@@ -0,0 +1,33 @@
+<?php
+
+/* FILE: update_link.php
+ *
+ * Used to put links in the LINK DB */
+// Includes
+include "../cats.php";
+// Passed Variables 
+$link_name = ucwords($_POST['link_name']);
+$link_cat = $_POST['link_cat'];
+$link_owner = $_POST['link_owner'];
+$link_url = $_POST['link_url'];
+$link_id = $_REQUEST['link_id'];
+$link_type = "";
+
+if (strpos($link_url, 'grantsgrabbag') !== false)
+{
+	$link_type= "internal";
+}
+else
+{
+	$link_type= "external";
+}
+$link_update_sql = ("UPDATE links SET link_name=\"".$link_name."\", link_cat=\"".$link_cat.", link_url=\"".$link_url."\", link_type=\"".$link_type."\" WHERE link_id=\"".$link_id."\"");
+$link_update = mysqli_query($links_conn, $link_ins_sql);
+
+if ($link_ins_sql) {
+	header ("Location: ./addforlinks.php");
+} else {
+	echo "ERROR: ".mysqli_error($links_conn);
+}
+
+?>
