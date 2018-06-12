94ac8bca0a0b288d26515fb9a6f95537f0aec153
gwirvin <grantirvin@aol.com>
2018-06-12 13:44:04 -0400

    Query name was wrong

--- links/update_link.php
+++ links/update_link.php
@@ -22,9 +22,9 @@ else
 	$link_type= "external";
 }
 $link_update_sql = ("UPDATE links SET link_name=\"".$link_name."\", link_cat=\"".$link_cat.", link_url=\"".$link_url."\", link_type=\"".$link_type."\" WHERE link_id=\"".$link_id."\"");
-$link_update = mysqli_query($links_conn, $link_ins_sql);
+$link_update = mysqli_query($links_conn, $link_update_sql);
 
-if ($link_ins_sql) {
+if ($link_update_sql) {
 	header ("Location: ./addforlinks.php");
 } else {
 	echo "ERROR: ".mysqli_error($links_conn);
