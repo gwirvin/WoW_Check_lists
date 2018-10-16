6eb017e2698d0279064e8c2e02217b3b81034fe6
Grant Irvin <grantirvin@aol.com>
2018-10-16 18:21:02 +0000

    Making it actually work

--- wow/includes/bfa_chk_lst.php
+++ wow/includes/bfa_chk_lst.php
@@ -26,18 +26,38 @@ if (!isset($_SESSION) || empty($_SESSION)) {
 	} else {
 	$toon_owner_first = $_SESSION['user_first'];
 	}
+#$dbhost = 'localhost';
+#$dbuser = 'web_app';
+#$dbpass = 'zoh6oePohb3a';
+#$dbusers = 'users';
+#$dbwow = 'wow_api';
+#$dblinks = 'links';
+#$users_conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbusers) or die   ('Could not connect to DB');
+#$wow_conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbwow) or die   ('Could not connect to DB');
+#$links_conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dblinks) or die ('Could not connect to the DB');
+//$wow_conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die   ('Could not connect to DB');
+#$db_user = mysqli_select_db($users_conn, $dbusers) or die ('Something went wrong!');
+#$db_wow = mysqli_select_db($wow_conn, $dbwow) or die ('Something went wrong!');
+#$db_links = mysqli_select_db($links_conn, $dblinks) or die ("Something went wrong!");
+#$api_key = "apikey=fkff3mjw67rm6eqzsf2u9vxgfk4y5b88";
+#$wow_url = "https://us.api.battle.net/wow/";
+#$icon_url = "http://render-us.worldofwarcraft.com/character/";
+#$blizz_locale = "locale=en_US";
 $toon = "";
 $toon_url = "";
 $db_toon_count = "";
 $db_toon_counter = 0;
 $wow_item_url = $wow_url."item/";
 $char_url = $wow_url."character/";
-$leg_char_fields = "fields=reputation,professions,talents,mounts,pets,titles,guild,items";
-$toon_sql = ("SELECT toonNmae, toon_realm FROM toon WHERE toon_owner=\"".$toon_owner_id."\" ORDER BY toon_realm, toonNmae");
+$wow_char_fields = "fields=reputation,professions,talents,mounts,pets,titles,guild,items";
+$toon_sql = ("SELECT toon_name, toon_realm FROM toon WHERE toon_owner=\"".$toon_owner_id."\" ORDER BY toon_realm, toon_name");
 $toon_query = mysqli_query($wow_conn, $toon_sql);
-$maxLgndLvl = 1000;
+$priProfCount=0;
+$toonName = "";
+$toonRealm = "";
+$toonIcon = "";
 // Starting the character table
-$toon_table = "<table>\n\t<caption><center><font color=\"FFFFFF\"><h3>Legion checklist for ".$toon_owner_first."</h3></font></center></caption>\n\t<thead>\n\t<tr>\n\t\t<th rowspan=\"2\"bgcolor=\"BFBCBA\">Realm</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Character</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Icon</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Level</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Current Spec</th>\n\t\t<th colspan=\"2\" rowspan=\"2\" bgcolor=\"6C00FF\"><font color=\"FFFFFF\">Primary Professions</font></th>\n\t\t<th colspan=\"4\" bgcolor=\"9E5FF4\"><font color=\"FFFFFF\">Secondary Professions</font></th>\n\t\t<th colspan=\"9\" bgcolor=\"0008FF\"><font color=\"FFFFFF\">Reputations</font></th>\n\t\t<th rowspan=\"2\" bgcolor=\"E6DC43\">Artifact Rank</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Equiped iLvl</th>\n\t</tr>\n\t<tr>\n\t\t<th bgcolor=\"F09456\">Needed Total</th>\n\t\t<th bgcolor=\"D38550\">Needs Lvl</th>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Cooking</font></th>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Fishing</font></th>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Archaeology</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Champions of Azeroth/font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Tortallan Seekers</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">The Honorbound\The 7<sup>th</sup> Legion</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Zandalari Empire/Proudmoore Admiralty</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Talanji's Expedition/Storm's Wake</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Voldunai/Order of Embers</font></th>\n\t</tr>\n\t</thead>\n\t<tbody>";
+$toon_table = "<table>\n\t<caption><center><font color=\"FFFFFF\"><h3>Legion checklist for ".$toon_owner_first."</h3></font></center></caption>\n\t<thead>\n\t<tr>\n\t\t<th rowspan=\"2\"bgcolor=\"BFBCBA\">Realm</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Character</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Icon</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Level</th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Current Spec</th>\n\t\t<th colspan=\"2\" rowspan=\"2\" bgcolor=\"6C00FF\"><font color=\"FFFFFF\">Primary Professions</font></th>\n\t\t<th colspan=\"3\" bgcolor=\"9E5FF4\"><font color=\"FFFFFF\">Secondary Professions</font></th>\n\t\t<th colspan=\"6\" bgcolor=\"0008FF\"><font color=\"FFFFFF\">Reputations</font></th>\n\t\t<th rowspan=\"2\" bgcolor=\"BFBCBA\">Equiped iLvl</th>\n\t</tr>\n\t<tr>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Cooking</font></th>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Fishing</font></th>\n\t\t<th bgcolor=\"B180F4\"><font color=\"FFFFFF\">Archaeology</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Champions of Azeroth</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Tortallan Seekers</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">The Honorbound\The 7<sup>th</sup> Legion</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Zandalari Empire/Proudmoore Admiralty</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Talanji's Expedition/Storm's Wake</font></th>\n\t\t<th bgcolor=\"5D63FF\"><font color=\"FFFFFF\">Voldunai/Order of Embers</font></th>\n\t</tr>\n\t</thead>\n\t<tbody>";
 // Getting the data from the database for the rest to work
 while ($toon_result = mysqli_fetch_all($toon_query, MYSQLI_ASSOC)) {
 	$db_toon_count = count($toon_result); // $db_toon_count is the total bumber of resutls, db_toon_counter increments on each run through the script
@@ -45,17 +65,18 @@ while ($toon_result = mysqli_fetch_all($toon_query, MYSQLI_ASSOC)) {
 /* Setting these variables at a higher level in the script did not get them emptied */
 // Variables that need some setting other than empty
 // Variables that should really be empty on each loop's start
-		$toonNmae = $toon_result[$db_toon_counter]['toonNmae'];
+		$toonName = $toon_result[$db_toon_counter]['toon_name'];
 		$toonRealm = $toon_result[$db_toon_counter]['toon_realm'];
-		$toon_info_url = $char_url.$toon_realm."/".$toonNmae."?".$leg_char_fields."&".$blizz_locale."&".$api_key; // The magic from the Blizzard API
-		$toonObj = json_decode(getToonInfo($toon_info_url));
+		$toon_info_url = $wow_url."character/".$toonRealm."/".$toonName."?fields=reputation,professions,talents,mounts,pets,titles,guild,items&".$blizz_locale."&".$api_key;
+		$toonInfo = getToonInfo($toon_info_url);
+		$toonObj = json_decode($toonInfo);
 		$toonFaction = $toonObj->faction;
 		$toonIcon = $icon_url.$toonObj->thumbnail;
 		$toonRealm = $toonObj->realm;
 		$toonTalents = $toonObj->talents;
-		$toon_realm_html = factionStylesRealm($toon_faction, $toon_realm);
-		$toon_icon_html = factionStylesIcon($toon_faction, $toon_icon, $toonNmae, $toon_realm);
-		$toonPriProfHtml = bfaPrimaryProfs($toonObj->professions->primary);
+		$toon_realm_html = factionStylesRealm($toonFaction, $toonRealm);
+		$toon_icon_html = factionStylesIcon($toonFaction, $toonIcon, $toonName, $toonRealm);
+		$toonPriProfHtml = bfaPrimaryProfs($toonObj->professions->primary, $priProfCount);
 		$toonSecProfHtml = bfaSecondaryProfs($toonObj->professions->secondary);
 		$toonRepHtml = bfaFactions($toonObj->reputation, $toonObj->faction);
 		$toon_bg_color = wowClassColors($toonObj->class);
@@ -64,7 +85,7 @@ while ($toon_result = mysqli_fetch_all($toon_query, MYSQLI_ASSOC)) {
 		$toonLvlCell = "\n\t\t<td ".$toonClassCellColor.$toonObj->level."</font></td>";
 		$toonSpecCell = "\n\t\t<td ".$toonClassCellColor.$toonObj->talents[0]->spec->name."</font></td>";
 		$toonIlvlCell = "\n\t\t<td ".$toonClassCellColor.$toonObj->items->averageItemLevelEquipped."</font></td>";
-		$toon_table .= "\n\t<tr>".$toon_realm_html.$toonNameCell.$toon_icon_html.$toonLvlCell.$toonSpecCell.$toonPriProfsHtml.$toonSecProfHtml.$toonRepHtml.$toonIlvlCell."\n\t</tr>\n";
+		$toon_table .= "\n\t<tr>".$toon_realm_html.$toonNameCell.$toon_icon_html.$toonLvlCell.$toonSpecCell.$toonPriProfHtml.$toonSecProfHtml.$toonRepHtml.$toonIlvlCell."\n\t</tr>\n";
 	}
 	sleep(0.15);
 }
--- wow/includes/wow_fact_inc.php
+++ wow/includes/wow_fact_inc.php
@@ -2,26 +2,31 @@
 /* file: wow_fact_inc.php 
  * This file gives us styles for showing the 
  * faction the character is in */
-function factionStylesRealm ($toon_faction, $toon_realm) {
-    switch ($toon_faction) {
-        case 1: //Putting a faction color on the realm name
-            return "\n\t\t<td bgcolor=\"C80A04\"><font color=\"FFFFFF\">".$toon_realm."</font></td>";
-        case  0:
-            return "\n\t\t<td bgcolor=\"2133E7\"><font color=\"FFFFFF\">".$toon_realm."</font></td>";
-        default:
-            return "\n\t\t<td bgcolor=\"EFDD05\"><font color=\"000000\">".$toon_realm."</font></td>";
-        }
-    }
+function factionStylesRealm ($toon_faction, $toonRealm) {
+	$return = "";
+	switch ($toon_faction) {
+		case 0:
+			$return = "\n\t\t<td bgcolor=\"2133E7\"><font color=\"FFFFFF\">".$toonRealm."</font></td>";
+			break;
+		case 1: //Putting a faction color on the realm name
+			$return =  "\n\t\t<td bgcolor=\"C80A04\"><font color=\"FFFFFF\">".$toonRealm."</font></td>";
+			break;
+		default:
+			$return  = "\n\t\t<td bgcolor=\"EFDD05\"><font color=\"000000\">".$toonRealm."</font></td>";
+			break;
+		}
+		return $return;
+	}
 
-function factionStylesIcon ($toon_faction, $toon_icon, $toon_name, $toon_realm) {
-    switch ($toon_faction) {
-        case 1: //Putting a faction color in the icon cell
-        return "\n\t\t<td bgcolor=\"C80A04\"><a href=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\"></a></td>";
-        case  0:
-        return "\n\t\t<td bgcolor=\"2133E7\"><a href=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\"></a></td>";
-        default:
-        return "\n\t\t<td bgcolor=\"EFDD05\"><a href=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toon_realm."/".$toon_name."/advanced\"></a></td>";
-         }
-    }
+function factionStylesIcon ($toon_faction, $toon_icon, $toonName, $toonRealm) {
+	switch ($toon_faction) {
+		case 1: //Putting a faction color in the icon cell
+			return "\n\t\t<td bgcolor=\"C80A04\"><a href=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\"></a></td>";
+		case	0:
+			return "\n\t\t<td bgcolor=\"2133E7\"><a href=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\"></a></td>";
+		default:
+			return "\n\t\t<td bgcolor=\"EFDD05\"><a href=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://us.battle.net/wow/en/character/".$toonRealm."/".$toonName."/advanced\"></a></td>";
+		}
+	}
 
 ?>
