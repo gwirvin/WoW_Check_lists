c32cd613abd925234e4df89fd2be60b5229bbd81
gwirvin <grantirvin@aol.com>
2018-10-16 11:39:22 -0400

    initial commit of file

---
+++ wow/includes/wow_html_inc.php
@@ -0,0 +1,16 @@
+<?php
+/* File: leg_html_inc.php */
+
+// Generic HTML Variables
+$html_head_open = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n\t<meta charset=\"UTF-8\">\r\n\t<link rel=\"stylesheet\" href=\"./style.css\">\r\n\t<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css\">\r\n\t<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>";
+$html_head_close = "\r\n</head>";
+$html_body_open = "\r\n<body>";
+$html_body_black_open = "\r\n<body bgcolor=\"000000\">";
+$html_div_close = "\r\n\t\t</div>";
+$html_select_close = "\r\n\t\t\t</select>";
+$html_form_close = "\r\n\t</form>";
+$html_body_close = "\r\n</body>";
+$html_footer_open = "\r\n<footer>";
+$html_footer_close = "\r\n</footer>";
+$html_close = "\r\n</html>\r\n";
+?>
