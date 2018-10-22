3fb0651abbfc29b5d3f878f0ab4a29d78d21cf3c
gwirvin <grantirvin@aol.com>
2018-10-22 13:50:05 -0400

    array_push did not add to , trying array_combine

--- wow/index.php
+++ wow/index.php
@@ -3,7 +3,7 @@ include "../cats.php";
 include "includes/blz_oauth_inc.php";
 print_r($SESSION);
 $oauthToken = getOauthToken($blizzardOauthUrl);
-array_push($_SESSION, $oauthToken);
+$_SESSION = array_combine($_SESSION, $oauthToken);
 print "\n";
 print_r($_SESSION);
 ?>
