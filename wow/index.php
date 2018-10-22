fe3c4709c8b1004e1bbd626821af09493808ee09
Grant Irvin <grantirvin@aol.com>
2018-10-22 18:51:25 +0000

    finding out how to get the oauth token

--- wow/index.php
+++ wow/index.php
@@ -1,9 +1,26 @@
 <?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['user_email']) || empty($_SESSION['user_email'])){
+  header("location: /login.php");
+  exit;
+}
 include "../cats.php";
-include "includes/blz_oauth_inc.php";
-print_r($SESSION);
+include "./includes/blz_oauth_inc.php";
+print "<br />";
+var_dump($_SESSION);
+print "<br />";
 $oauthToken = getOauthToken($blizzardOauthUrl);
-$_SESSION = array_combine($_SESSION, $oauthToken);
-print "\n";
-print_r($_SESSION);
+print "<br />";
+var_dump($oauthToken);
+print "<br />";
+#$_SESSION['oauth'] = $oauthToken;
+array_push($_SESSION, getOauthToken($blizzardOauthUrl));
+print "<br />";
+var_dump($_SESSION);
+print "<br />";
+print_r($_SESSION['oauth']['access_token']);
+print "<br />";
 ?>
