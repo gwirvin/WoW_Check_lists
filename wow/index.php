e43815d5639aefbbd733adc06e837131d9a780f0
gwirvin <grantirvin@aol.com>
2018-10-23 14:05:44 -0400

    more testing, is there something in

--- wow/index.php
+++ wow/index.php
@@ -11,5 +11,14 @@ include "../cats.php";
 include "./includes/blz_oauth_inc.php";
 $_SESSION = array_merge($_SESSION, getOauthToken($blizzardOauthUrl));
 
+print "<hr />";
+print "Your toekn is: ".$_SESSION['access_token'];
+print "<hr />";
+print "<a href=\"".$blizzardOauthAuthUrl."/?response_type=code&clientid=".$clientId."&redirect_uri=".$wowIndexUri."&scope=wow.profile&state=1234xyz\">Blizzard Login</a>";
+print "<hr />";
+var_dump($_SESSION);
+print "<hr />";
+var_dump($_GET);
+print "<hr />";
 
 ?>
