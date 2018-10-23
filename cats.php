5307adf0c68e8db89c1cc30c1a58a83d671ddafe
gwirvin <grantirvin@aol.com>
2018-10-23 13:54:09 -0400

    made variables for redirects

--- cats.php
+++ cats.php
@@ -24,7 +24,13 @@ $api_key = "apikey=fkff3mjw67rm6eqzsf2u9vxgfk4y5b88";
 $wow_url = "https://us.api.battle.net/wow/";
 $icon_url = "http://render-us.worldofwarcraft.com/character/";
 $blizz_locale = "locale=en_US";
-$blizzardOauthUrl = "https://us.battle.net/oauth/token";
+$blizzardOauthTokenUrl = "https://us.battle.net/oauth/token";
+$blizzardOauthAuthUrl = "https://us.battle.net/oauth/authorize";
+$secret = "4lDDBAihtzLRkljrtYmwRTL1L6xZeOGv";
+$clientId = "2abc5bd3a1d64274a2aa9575404cf0e9";
+$latestUri = "https://www.grantsgrabbag.com/wow/latest.php";
+$wowUri -"https://www.grantsgrabbag.com/wow/wow.php";
+$wowIndexUri = "https://www.grantsgrabbag.com/wow/index.php";
 
 // Navigation table
 $nav_link_sql = mysqli_query($links_conn, "SELECT link_type, link_name, link_url FROM links WHERE link_cat=\"Navigation\" AND link_owner=\"".$_SESSION['user_id']."\" ORDER BY link_name") or die(mysqli_error());
