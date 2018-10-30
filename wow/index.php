7108ccfa5ae18a19c4dfa267c8228ee0f7e4a21d
gwirvin <grantirvin@aol.com>
2018-10-30 14:28:33 -0400

    Once more adding Ulminia's OAuth stuff in

--- wow/index.php
+++ wow/index.php
@@ -1,27 +1,47 @@
 <?php
-// Initialize the session
-session_start();
+/**
+*
+*	version 1.4
+*/
+error_reporting(E_ALL);
+ ini_set('display_errors', 1);
 
-// If session variable is not set it will redirect to login page
-if(!isset($_SESSION['user_email']) || empty($_SESSION['user_email'])){
-  header("location: /login.php");
-  exit;
+
+require_once('Client.php');
+require_once('GrantType/IGrantType.php');
+require_once('GrantType/AuthorizationCode.php');
+
+/**
+*	Required vars for the api to work
+*
+**/
+
+$client_id			= '2abc5bd3a1d64274a2aa9575404cf0e9';
+$client_secret			= '4lDDBAihtzLRkljrtYmwRTL1L6xZeOGv';
+$region				= 'US';
+$locale				= 'en_US';
+$redirect_uri			= 'https://www.grantsgrabbag.com/wow/index.php';
+
+// init the auth system client_id, client_secret, region, local all required
+$client = new OAuth2\Client($client_id, $client_secret, $region, $locale, $redirect_uri);
+
+
+if (!isset($_GET['code']))
+{
+	$auth_url = $client->getAuthenticationUrl($client->baseurl[$client->region]['AUTHORIZATION_ENDPOINT'], $client->redirect_uri);
+	//echo $auth_url;
+	header('Location: ' . $auth_url);
 }
-include "../cats.php";
-include "./includes/blz_oauth_inc.php";
-$_SESSION = array_merge($_SESSION, getOauthToken($blizzardOauthUrl));
-
-print "<hr />";
-print "Your toekn is: ".$_SESSION['access_token'];
-print "<hr />";
-print "<a href=\"".$blizzardOauthAuthUrl."/?response_type=code&clientid=".$clientId."&redirect_uri=".$wowIndexUri."&scope=wow.profile&state=1234xyz\">Blizzard Login</a>";
-print "<hr />";
-print $blizzardOauthAuthUrl."/?response_type=code&clientid=".$clientId."&redirect_uri=".$wowIndexUri."&scope=wow.profile&state=1234xyz";
-print "<hr />";
-var_dump($_SESSION);
-print "<hr />";
-var_dump($_GET);
-print "<hr />";
-$profileApiCall = file_get_contents("https://us.battle.net/wow/user/characters");
-var_dump($profileApiCall);
-?>
+else
+{
+	$params = array('code' => $_GET['code'], 'auth_flow' => 'auth_code', 'redirect_uri' => $client->redirect_uri);
+	$response = $client->getAccessToken($client->baseurl[$client->region]['TOKEN_ENDPOINT'], 'authorization_code', $params);
+echo '<pre>';
+print_r($response);
+echo '</pre>';
+	$client->setAccessToken($response['result']['access_token']);
+	$response = $client->fetch('user',array('source'=>'account'));
+	echo '<pre>';
+	print_r($response);
+	echo '</pre>';
+}
