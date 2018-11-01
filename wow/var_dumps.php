ab58d553a087e056f99e72eb1bbf3f75993ffd3d
gwirvin <grantirvin@aol.com>
2018-11-01 13:31:10 -0400

    missed changing back to redirect_uri's

--- wow/var_dumps.php
+++ wow/var_dumps.php
@@ -30,15 +30,15 @@ $client_id			= '2abc5bd3a1d64274a2aa9575404cf0e9';
 $client_secret			= '4lDDBAihtzLRkljrtYmwRTL1L6xZeOGv';
 $region				= 'US';
 $locale				= 'en_US';
-$redirectUri			= 'https://www.grantsgrabbag.com/wow/var_dumps.php';
+$redirect_uri			= 'https://www.grantsgrabbag.com/wow/var_dumps.php';
 
 // init the auth system client_id, client_secret, region, local all required
-$client = new OAuth2\Client($client_id, $client_secret, $region, $locale, $redirectUri);
+$client = new OAuth2\Client($client_id, $client_secret, $region, $locale, $redirect_uri);
 
 $myOauthToken = getOauthToken($blizzardOauthTokenUrl);
 print "var_dump of \$myOauth:\n<pre>"; var_dump($myOauthiToken); print "</pre>\n<hr />\n";
 
-$myOauthCode = getOauthCode($blizzardOauthAuthUrl, $redirectUri, $_SESSION['access_token']);
+$myOauthCode = getOauthCode($blizzardOauthAuthUrl, $redirect_uri, $_SESSION['access_token']);
 print "var_dump of \$myOauth:\n<pre>"; var_dump($myOauthCode); print "</pre>\n<hr />\n";
 
 print "var_dump of \$_SESSION:\n<pre> "; var_dump($_SESSION); print "</pre>>\n<hr />\n";
@@ -49,13 +49,13 @@ print "var_dump of \$_GET:\n<pre>"; var_dump($_GET); print "</pre>>\n<hr />\n";
 
 if (!isset($_GET['code']))
 {
-	$auth_url = $client->getAuthenticationUrl($client->baseurl[$client->region]['AUTHORIZATION_ENDPOINT'], $client->redirectUri);
+	$auth_url = $client->getAuthenticationUrl($client->baseurl[$client->region]['AUTHORIZATION_ENDPOINT'], $client->redirect_uri);
 	print "var_dump of \$auth_url:<pre>"; var_dump($auth_url); print "</pre>\n<hr />\n";
 	header('Location: ' . $auth_url);
 }
 else
 {
-	$params = array('code' => $_GET['code'], 'auth_flow' => 'auth_code', 'redirect_uri' => $client->redirectUri);
+	$params = array('code' => $_GET['code'], 'auth_flow' => 'auth_code', 'redirect_uri' => $client->redirect_uri);
 	print "var_dump of \$params:\n<pre>";
 	var_dump($params);
 	print "</pre>";
