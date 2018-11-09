196570663868b47920859fc2dcbad08fbfdafa49
gwirvin <grantirvin@aol.com>
2018-11-09 10:36:05 -0500

    putting a link BACK in to directly call for a Auth Token

--- wow/var_dumps.php
+++ wow/var_dumps.php
@@ -39,6 +39,12 @@ $myOauthToken = getOauthToken($blizzardOauthTokenUrl);
 print "var_dump of \$myOauthToken:\n<pre>"; var_dump($myOauthToken); print "</pre>\n<hr />\n";
 
 $myOauthCode = getOauthCode($blizzardOauthAuthUrl, $redirect_uri, $myOauthToken['access_token']);
+
+print "<a href=\"".$blizzardOauthAuthUrl."/?response_type=code&clientid=".$clientId."&redirect_uri=".$wowIndexUri."&scope=wow.profile&state=1234xyz\">Blizzard Login</a>";
+print "<hr />";
+print $blizzardOauthAuthUrl."/?response_type=code&clientid=".$clientId."&redirect_uri=".$wowIndexUri."&scope=wow.profile&state=1234xyz";
+print "<hr />";
+
 print "var_dump of \$myOauthCode:\n<pre>"; var_dump($myOauthCode); print "</pre>\n<hr />\n";
 
 print "var_dump of \$_SESSION:\n<pre> "; var_dump($_SESSION); print "</pre>>\n<hr />\n";
