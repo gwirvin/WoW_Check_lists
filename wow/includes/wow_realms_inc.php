81cb00912a5cbabd66caab90b75de0ec3decf9e7
Grant Irvin <grantirvin@aol.com>
2019-02-28 17:07:06 -0500

    Update for Oauth: bad variable name, again, again

--- wow/includes/wow_realms_inc.php
+++ wow/includes/wow_realms_inc.php
@@ -7,14 +7,14 @@
  * */
 
 function wowRealmList ($realmsUrl) {
-	$realmSelect = "";
+	$realmSelectHtml = "";
 	$realmsObject = json_decode(file_get_contents($realmsUrl));
 	foreach ($realmsObject->realms as $realmsValue) {
 		$realmName = $realmsValue->name;
 		$realmSlug = $realmsValue->slug;
 		$realmSelectHtml .= "\r\n\t\t\t\t<option value=\"".$realmSlug."\">".$realmName."</option>";
 	}
-	return $realmSelect;
+	return $realmSelectHtml;
 }
 
 ?>
