d23775f3914b5dd79ae126339d4008d11c06cebc
Grant Irvin <grantirvin@aol.com>
2019-03-07 16:55:18 -0500

    using _SESSION variable

--- wow/var_dumps.php
+++ wow/var_dumps.php
@@ -40,11 +40,11 @@ print "Value of \$wowIndexUri is: ".$wowIndexUri."<br />Value with urlencode is:
 $myOauthToken = getOauthToken($blizzardOauthTokenUrl);
 
 print "var_dump of \$myOauthToken:\n<pre>"; var_dump($myOauthToken); print "</pre>\n<hr />\n";
-$oauthFetchSql = ("SELECT oauth_user, oauth_token, oauth_token_expires FROM oauth_session WHERE ouath_user=\"".$user_id."\"");
+$oauthFetchSql = ("SELECT oauth_user, oauth_token, oauth_token_expires FROM oauth_session WHERE ouath_user=\"".$$_SESSION['user_id']."\"");
 print "\rvar_dump of \$oauthFetchSql:\r<pre>"; var_dump($oauthFetchSql); print "\r</pre>\r<hr />\r";
 $oauthCheckQuery = mysqli_query($wow_conn, $oauthFetchSql);
 if(!isset($oauthCheckQuery['oauth_token']) || empty($oauthCheckQuery['oauth_token'])) {
-	$oauthInsertSql = ("INSERT INTO oauth_session (oauth_user, oauth_token, oauth_token_expires) VALUES (\"".$myOauthToken['access_token']."\", \"".$user_id."\", NOW()) ON DUPLICATE KEY UPDATE");
+	$oauthInsertSql = ("INSERT INTO oauth_session (oauth_user, oauth_token, oauth_token_expires) VALUES (\"".$myOauthToken['access_token']."\", \"".$$_SESSION['user_id']."\", NOW()) ON DUPLICATE KEY UPDATE");
 	print "var_dump of \$oauthInsertSql:\r<pre>\r"; var_dump($oauthInsertSql); print "\r</pre>\r<hr />\r";
 #	$oauthInsertQuery = mysqli_query($wow_conn, $oauthInsertSql);
 }
