cb1a47668221b44c12c4ba3d5b6fd54febf05230
gwirvin <grantirvin@aol.com>
2019-08-22 13:32:40 -0400

    Rep levels again (#15)

--- wow/includes/wow_reps_inc.php
+++ wow/includes/wow_reps_inc.php
@@ -116,10 +116,21 @@ function bfaFactions($bfaReps, $wowFaction) {
 					break;
 			}
 		} elseif ($bfaRep->id === 2392) {
+					} elseif ($bfaRep->id === 2392) {
 			switch ($bfaRep->standing) {
-				case 7:
+				case 0:
+					$rustboltParagon = "\n\t\t<td bgcolor=\"F1D50E\">".($bfaRep->max-$bfaRep->value)." to Paragon.</td>";
+					break;
+				case 1:
+					$rustboltParagon = "\n\t\t<td bgcolor=\"BDF10E\">".($bfaRep->max-$bfaRep->value)." to Paragon.</td>";
+					break;
+				case 2:
+					$rustboltParagon = "\n\t\t<td bgcolor=\"98F10E\">".($bfaRep->max-$bfaRep->value)." to Paragon.</td>";
+					break;
+				case 3:
 					$rustboltParagon = "\n\t\t<td bgcolor=\"5AF10E\">".($bfaRep->max-$bfaRep->value)." to Paragon.</td>";
 					break;
+
 				case NULL:
 					$rustboltParagon = "\n\t\t<td bgcolor=\"10AA06\"><font color=\"FFFFFF\">".$bfaRep->name."</font></td>";
 					break;
