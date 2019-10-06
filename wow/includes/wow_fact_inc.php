4694d5fdd004e459df1df5e0a5201d1f4e5cc54b
Grant Irvin <grantirvin@aol.com>
2019-10-06 16:05:39 +0000

    removing the advanced tag

--- wow/includes/wow_fact_inc.php
+++ wow/includes/wow_fact_inc.php
@@ -21,11 +21,11 @@ function factionStylesRealm ($toon_faction, $toonRealm) {
 function factionStylesIcon ($toon_faction, $toon_icon, $toonName, $toonRealmSlug) {
 	switch ($toon_faction) {
 		case 1: //Putting a faction color in the icon cell
-			return "\n\t\t<td bgcolor=\"C80A04\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\"></a></td>";
+			return "\n\t\t<td bgcolor=\"C80A04\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\"></a></td>";
 		case	0:
-			return "\n\t\t<td bgcolor=\"2133E7\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\"></a></td>";
+			return "\n\t\t<td bgcolor=\"2133E7\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\"></a></td>";
 		default:
-			return "\n\t\t<td bgcolor=\"EFDD05\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."/advanced\"></a></td>";
+			return "\n\t\t<td bgcolor=\"EFDD05\"><a href=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\" target=\"_blank\"><img src=\"".$toon_icon."\" style=\"height:30px;width:30px;border:1;\" alt=\"http://worldofwarcraft.com/en-us/character/us/".$toonRealmSlug."/".$toonName."\"></a></td>";
 		}
 	}
 
