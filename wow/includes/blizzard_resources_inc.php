c7146d2b05b9d0abcac1605fcdc47028594ecda2
Grant Irvin <grantirvin@aol.com>
2019-12-20 13:36:21 -0500

    trying to get prepared for new API methods

--- wow/includes/blizzard_resources_inc.php
+++ wow/includes/blizzard_resources_inc.php
@@ -6,11 +6,13 @@
  * This file is a prepository of all the things needed for use of the blizzard APIs
 */
 $wow_url = "https://us.api.battle.net/wow/";
-$wowUrl = "https://us.api.blizzard.com/wow/";
+$wowUrl = "https://us.api.blizzard.com/profile/wow/character/";
 $blizzardOauthTokenUrl = "https://us.battle.net/oauth/token";
 $blizzardOauthAuthUrl = "https://us.battle.net/oauth/authorize";
 $blizzardUserInfoUrl = "https://us.battle.net/oauth/user/info";
 $blizzardWowProfileUrl = "https://us.api.blizzard.com/wow/user/characters?locale=en_US";
+$blizzardProfileNamespace = "namespace=profile-us";
+$blizzardLocaleUs = "locale=en_US";
 $clientId = "2abc5bd3a1d64274a2aa9575404cf0e9";
 $clientSecret = "4lDDBAihtzLRkljrtYmwRTL1L6xZeOGv";
 $blizzardAuthorizeUrl = "https://us.battle.net/oauth/authorize";
@@ -22,6 +24,25 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://us.api.blizzard.com/wow/user/characters?locale=en_US");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+
+* EXAMPLE URIs for new API methods
+* Profile Summary: /profile/wow/character/{realmSlug}/{characterName}
+	Luxalor Profile summary: https://us.api.blizzard.com/profile/wow/character/area-52/luxalor?namespace=profile-us&locale=en_US&access_token=<OAUTH_TOKEN>
+	INCLUDES, Level, Spec, Race, Faction, active title and other Profile URLs
+* Profile Status: /profile/wow/character/{realmSlug}/{characterName}/status
+	Luxchen Profile Status: https://us.api.blizzard.com/profile/wow/character/area-52/luxchen/status?namespace=profile-us&locale=en_US&access_token=<OAUTH_TOKEN>
+	USE TO REMOVE INVALID CHARACTERS
+* Reputations: /profile/wow/character/{realmSlug}/{characterName}/reputations
+	Luxdino Reps: https://us.api.blizzard.com/profile/wow/character/area-52/luxdino/reputations?namespace=profile-us&locale=en_US&access_token=<OAUTH_TOKEN>
+* Professions: NO NEW METHOD YET
+	 No updated API for professions!!!
+* Titles: /profile/wow/character/{realm-slug}/{character-name}/titles
+	Luxdief Titles: https://us.api.blizzard.com/profile/wow/character/proudmoore/luxdief/titles?namespace=profile-us&locale=en_US&access_token=<OAUTH_TOKEN>
+	* Specializations: /profile/wow/character/{realmSlug}/{characterName}/specializations
+	Luxtodes Specializations: https://us.api.blizzard.com/profile/wow/character/area-52/luxtodes/specializations?namespace=profile-us&locale=en_US&access_token=<OAUTH_TOKEN>
+
+
 
 
 $headers = array();
