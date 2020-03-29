bf582ec8f031c71dcc918208b4f2b22a8a7c1d87
Grant Irvin <grantirvin@aol.com>
2020-03-29 00:37:28 +0000

    Updated wowRealmList function

--- wow/includes/wow_realms_inc.php
+++ wow/includes/wow_realms_inc.php
@@ -9,9 +9,13 @@
 function wowRealmList ($realmsUrl) {
 	$realmSelectHtml = "";
 	$realmsObject = json_decode(file_get_contents($realmsUrl));
-	foreach ($realmsObject->realms as $realmsValue) {
-		$realmName = $realmsValue->name;
-		$realmSlug = $realmsValue->slug;
+	$realmsCount = count($realmsObject->realms);
+	for ( $realmsCounter = 0; $realmsCounter < $realmsCount; $realmsCounter++) {
+		$realmName = $realmsObject->realms[$realmsCounter]->name;
+		$realmSlug = $realmsObject->realms[$realmsCounter]->slug;
+//	foreach ($realmsObject->realms as $realmsValue) {
+//		$realmName = $realmsValue->name;
+//		$realmSlug = $realmsValue->slug;
 		$realmSelectHtml .= "\r\n\t\t\t\t<option value=\"".$realmSlug."\">".$realmName."</option>";
 	}
 	return $realmSelectHtml;
