b8fe439db2b791e8eb76bbee646fd13b213c0572
gwirvin <grant.irvin@verizonmedia.com>
2020-10-20 11:14:40 -0400

    Getting an all character page working (#24)

--- wow/character.php
+++ wow/character.php
@@ -11,6 +11,27 @@ if(!isset($_SESSION['user_email']) || empty($_SESSION['user_email'])){
 }
 
 
+// Block for testing
+/*
+*/
+include "../cats.php";
+include "./includes/blz_oauth_inc.php";
+include "includes/blizzard_resources_inc.php";
+include "./includes/wow_char_inc.php";
+include "./includes/wow_class_inc.php";
+include "./includes/wow_fact_inc.php";
+include "./includes/wow_html_inc.php";
+include "./includes/wow_profs_inc.php";
+include "./includes/wow_reps_inc.php";
+include "./includes/user_chars_inc.php";
+#include "./includes/multiapi.php";
+
+// Variables for the start
+$toon_owner_id = $_SESSION['user_id'];
+$toon_owner_first = $_SESSION['user_first'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,7 +52,75 @@ if(!isset($_SESSION['user_email']) || empty($_SESSION['user_email'])){
             <div class="form-group"><td><center><form name="legion" method="POST" action="./latest.php"><input value="Legion - <?php echo $_SESSION['user_first']; ?>" type="submit"></form></td></div>
             <div class="form-group"><td><center><form name="toon" method="POST" action="./wow.php"><input value="<?php echo $_SESSION['user_first']; ?>'s Characters" type="submit"></form></td></div>
     </div>
-    <h1><center> Hey, <?php echo $_SESSION['user_first']?>, the web developer has this page here with no idea what to do with it. Sorry.</center></h2>
+<!--<hr /> -->
+    <h2><center>World of Warcraft: <?php echo $_SESSION['user_first']?>'s Characters</center></h2>
+<!--    <div id="container"> -->
+    <div id="table-scroll">
+<script>
+$(document).ready(function() {
+    $("table").tablesorter({
+      sortList: [[0,0],[3,0]]
+      });
+   });
+</script>
+        <?php print $basicToonTable?>
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("table-scroll");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
+    </div>
     <div id="table-nav">
         <table>
             <tr>
